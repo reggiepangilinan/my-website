b9a76a7aad054c310b21e5bb97603a2007f97dd8
Reggie Pangilinan <reggie.pangilinan@zip.co>
2021-04-17 17:57:32 +1000

    Test commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Reggie Pangilinan`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@reggiepangilinan`,
   },
   plugins: [
     `gatsby-plugin-sass`,
