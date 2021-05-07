407338428e2ab985aab8954a4d025f1dc1074f6c
Reggie Pangilinan <reggie.pangilinan@zip.co>
2021-05-07 23:09:20 +1000

    Use gatsby delivery api

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,8 +13,7 @@ module.exports = {
       resolve: `gatsby-source-contentful`,
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-        host: process.env.CONTENTFUL_HOST
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
       },
     },
     {
